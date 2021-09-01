6b245348ac7f22a0905dccb223bfbf0f11706029
Mario Pilar <mrpilar@gmail.com>
2021-09-01 10:23:51 -0300

    projeto atualizado

--- src/main/java/com/example/desafioneurotech/desafioneurotech/controller/UsuarioController.java
+++ src/main/java/com/example/desafioneurotech/desafioneurotech/controller/UsuarioController.java
@@ -1,13 +1,11 @@
 package com.example.desafioneurotech.desafioneurotech.controller;
 
 import java.net.URI;
-
-import java.util.Optional;
-
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import com.example.desafioneurotech.desafioneurotech.model.Usuario;
@@ -37,13 +36,10 @@ public class UsuarioController {
 	}
 
 	@GetMapping("/{id}")
-	public ResponseEntity<Usuario> findById(@PathVariable Long id) {
-		Optional<Usuario> usuario = usuarioRepository.findById(id);
-		
-		if (usuario.isPresent()) {
-			return ResponseEntity.ok(usuario.get());
-		}
-		return ResponseEntity.notFound().build();
+	public Usuario findById(@PathVariable Long id) {
+		return usuarioRepository.findById(id)
+                .orElseThrow(() -> 
+                new ResponseStatusException(HttpStatus.NOT_FOUND, "Usuário não encontrado!"));
 	}
 
 }
